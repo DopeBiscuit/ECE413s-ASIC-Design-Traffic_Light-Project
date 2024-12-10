bad3c748f2c4c4e6daaabfbc3c070d6ac1113116
Rana Ayman <ranaayman2468@gmail.com>
2024-12-10 17:50:37 +0200

    modified testbench

--- tests/traffic_system_package.sv
+++ tests/traffic_system_package.sv
@@ -3,21 +3,53 @@ package traffic_system_package;
 	class traffic_system_class;
 
 		rand logic [3:0] _1st ,_5th ;
-		logic clk; 
-		rand logic rst;
+		bit clk; 
+		rand bit rst;
 		logic [3:0] red, green, yellow;
 
 		constraint reset {
 		rst dist{0:/2,1:/98};	
 		}
 
+		constraint _1st_5th {
+			(~_1st[0]) -> _5th[0]==0;
+			(~_1st[1]) -> _5th[1]==0;
+			(~_1st[2]) -> _5th[2]==0;
+			(~_1st[3]) -> _5th[3]==0;
+		}
+
+		constraint ss {
+		_1st dist{0:/10,[1:15]:/90};	
+		}
+
+		constraint fs {
+		_5th dist{0:/70,[1:15]:/30};	
+		}
+
 		covergroup cg @(posedge clk);
 
 			coverpoint _1st;
 			coverpoint _5th;
-			coverpoint red;
-			coverpoint green;
-			coverpoint yellow;
+			coverpoint red{
+				bins t1 = {4'b1110};
+				bins t2 = {4'b1101};
+				bins t3 = {4'b1011};
+				bins t4 = {4'b0111};
+			}
+			coverpoint green{
+				bins t1 = {4'b0001};
+				bins t2 = {4'b0010};
+				bins t3 = {4'b0100};
+				bins t4 = {4'b1000};
+				bins zero = {4'b0000};
+			}
+			coverpoint yellow{
+				bins t1 = {4'b0001};
+				bins t2 = {4'b0010};
+				bins t3 = {4'b0100};
+				bins t4 = {4'b1000};
+				bins zero = {4'b0000};
+			}
 
 		endgroup : cg
 
--- tests/traffic_system_tb.sv
+++ tests/traffic_system_tb.sv
@@ -7,10 +7,19 @@ module traffic_system_tb;
 
 	int error_count ,correct_count;
 
+	int i ;
+
     traffic_system_class ts;
 
-	traffic_system DUT(.*);
-	//bind  traffic_system traffic_system_SVA sva_inst(.*);
+    
+	traffic_system DUT(.clk(clk),.rst_n(rst_n),
+        .input_ss(input_ss),
+        .input_fs(input_fs),
+        .output_green(input_green),
+        .output_red(input_red),
+        .output_yellow(input_yellow));
+	
+
 
 	initial begin
 		clk = 0;
@@ -20,23 +29,35 @@ module traffic_system_tb;
 		end 
 	end
 
+	
+	 //check reset
+	always_comb begin 
+		if(~rst_n)
+			assert_rst: assert final(input_red == 4'b1110 && input_green == 4'b0001 && input_yellow == 4'b0000 );
+	end
+
 	initial begin
+		ts = new();
 		error_count = 0;
 		correct_count = 0;
+		i =0;
 		rst_n = 0;
 		input_ss = 0;
 		input_fs = 0;
 
-		ts = new();
+		reference_model(ts);
 
-
-		repeat(100) begin
+		repeat(1000) begin
+			assert(ts.randomize())
 			@(negedge clk);
-			assert(ts.randomize());
 			input_ss = ts._1st;
 			input_fs = ts._5th;
 			rst_n = ts.rst;
-			reference_model(ts);	
+			ts.red = input_red;
+			ts.green = input_green;
+			ts.yellow = input_yellow;
+			reference_model(ts);
+			check_output(ts);	
 		end
 
 		$display("at end of simulation error_count = %d and correct_count = %d",error_count, correct_count);
@@ -47,8 +68,8 @@ module traffic_system_tb;
 	task check_output(traffic_system_class t);
 		if(t.red !== input_red && t.green !== input_green && t.yellow !== input_yellow) begin
 			error_count++;
-			$display("error at %t 1st sensor = %b 5th sensor = %b red = %b green = %b yellow = %b"
-				,$time(), input_ss, input_fs, input_red, input_green, input_yellow);	
+			$display("error at %t 1st sensor = %b 5th sensor = %b red = %b green = %b yellow = %b exp_r=%b exp_g=%b exp_y=%b"
+				,$time(), input_ss, input_fs, input_red, input_green, input_yellow, t.red, t.green ,t.yellow);	
 			end
 		else
 			correct_count++;
@@ -56,17 +77,37 @@ module traffic_system_tb;
 	endtask : check_output
 
 	task reference_model(traffic_system_class t_ref);
-		int i ;
-
-		for(i=0 ; i<=10 ; i++) begin
-
+		
 			if(~rst_n) begin
-				t_ref.red = 4'b1110;
-				t_ref.green = 4'b0001;
-				t_ref.yellow = 4'b0000;	
+				repeat(30) begin
+						@(posedge clk);
+						t_ref.red = 4'b1111;
+						t_ref.green = 4'b0000;
+						t_ref.yellow = 4'b0000;
+						t_ref.red[0] = 0;
+						t_ref.green[0] = 1;	
+					end 
+					repeat(3) begin
+						@(posedge clk);
+						t_ref.red = 4'b1111;
+						t_ref.green = 4'b0000;
+						t_ref.yellow = 4'b0000;
+						t_ref.red[0] = 0;
+						t_ref.yellow[0] = 1;					
+					end	
 			end
 
 			else begin
+
+					if(~t_ref.red[0])
+				    i = 1;
+				    else if (~t_ref.red[1]) 
+				    i = 2;
+				    else if (~t_ref.red[2])
+				    i = 3;
+				    else
+				    i = 0;
+
 				if(input_ss[i])begin
 
 				 if(input_fs[i])begin
@@ -79,6 +120,7 @@ module traffic_system_tb;
 						t_ref.green[i] = 1;	
 					end 
 					repeat(3) begin
+						@(posedge clk);
 						t_ref.red = 4'b1111;
 						t_ref.green = 4'b0000;
 						t_ref.yellow = 4'b0000;
@@ -97,6 +139,7 @@ module traffic_system_tb;
 						t_ref.green[i] = 1;	
 					end 
 					repeat(3) begin
+						@(posedge clk);
 						t_ref.red = 4'b1111;
 						t_ref.green = 4'b0000;
 						t_ref.yellow = 4'b0000;
@@ -109,15 +152,9 @@ module traffic_system_tb;
 		        else begin
 		    	i = (i + 1) % 4;		
 		        end
-
-		    i = (i + 1) % 4;
 				
-			end
-
-			check_output(t_ref);
-	
 		end
-		
+	
 	endtask
 
 endmodule : traffic_system_tb
